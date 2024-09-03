66721d210e4e1c63cd9189965115fa6175ad0163
Diana <56115660+dianaventura@users.noreply.github.com>
2024-09-03 20:08:35 +0100

    Update 9-PalindromeNumber.py

--- 9-PalindromeNumber/9-PalindromeNumber.py
+++ 9-PalindromeNumber/9-PalindromeNumber.py
@@ -1,27 +1,19 @@
-        :rtype: bool
-        """
-        # A negative number can't be a palindrome
-        # Also, any number that ends with 0 cannot be a palindrome, except 0 itself
-        if x < 0 or (x % 10 == 0 and x != 0):
-            return False
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        # A negative number can't be a palindrome
+        # Also, any number that ends with 0 cannot be a palindrome, except 0 itself
+        if x < 0 or (x % 10 == 0 and x != 0):
+            return False
 
-        # Revert half of the number to check for palindrome
-        reversed_half = 0
-        while x > reversed_half:
-            reversed_half = reversed_half * 10 + x % 10
-            x //= 10
+        # Revert half of the number to check for palindrome
+        reversed_half = 0
+        while x > reversed_half:
+            reversed_half = reversed_half * 10 + x % 10
+            x //= 10
 
-        :type x: int
-    def isPalindrome(self, x):
-        """
-class Solution(object):
-1
-121
--121
-10
-true
-false
-false
-true
-false
-false
+        # If the number is odd, discard the middle digit
+        return x == reversed_half or x == reversed_half // 10
